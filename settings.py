a885c2c9aff3ef45bdd97cd51a3802e422cc2f83
Brian Hicks <therealcaffeine@gmail.com>
2011-04-12 23:53:14 -0500

    added south and the requirements file

--- settings.py
+++ settings.py
@@ -109,16 +109,20 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
+    # django contrib
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    
+    # third party
+    'south'
+    
+    # my stuff
 )
 
 # A sample logging configuration. The only tangible logging
